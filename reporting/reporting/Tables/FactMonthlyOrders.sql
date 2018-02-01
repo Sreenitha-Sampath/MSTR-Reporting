0d5caf930c5f214d23ef48bc88fd12def83f8049
Sreenitha Sampath <sreenitha.sampath07@gmail.com>
2018-02-01 09:32:27 +0530

    20.SSDT - Modified

--- reporting/reporting/Tables/FactMonthlyOrders.sql
+++ reporting/reporting/Tables/FactMonthlyOrders.sql
@@ -3,7 +3,7 @@
 	[LocationId] INT NOT NULL, 
     [VendorId] INT NOT NULL, 
 	[Month] INT NOT NULL, 
-    [PurchaseAmount] INT NOT NULL,
+    [PurchaseAmount] MONEY NOT NULL,
     [OrderCount] INT NOT NULL, 
 	CONSTRAINT PK_FactMonthlyOrders_LocationId_VendorId_Month PRIMARY KEY CLUSTERED ([LocationId],[VendorId],[Month]),
 	CONSTRAINT [FK_FactMonthlyOrders_Location_LocationId] FOREIGN KEY ([LocationId]) REFERENCES [DimLocation]([LocationId]),
