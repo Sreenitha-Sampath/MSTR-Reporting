31bdf213e30359b66920a5263d787262265e17a7
Sreenitha Sampath <sreenitha.sampath07@gmail.com>
2018-02-05 11:14:55 +0530

    20. SSDT - Modified

--- reporting/reporting/Stored Procedures/DimMonthData.sql
+++ reporting/reporting/Stored Procedures/DimMonthData.sql
@@ -1,21 +1,16 @@
-﻿
-
-CREATE PROCEDURE [dbo].[DimMonthData]
-
+﻿CREATE PROCEDURE [dbo].[DimMonthData]
 AS
 BEGIN
-	
 	DECLARE @StartDate date= '20100101'
-
 	WHILE @StartDate <= '20151231'
-		BEGIN
-			INSERT INTO dbo.DimMonth
-			(
-				MonthId, MonthName, Year
-			)
-			VALUES ( CAST ( CAST( YEAR(@StartDate) AS VARCHAR(10) ) + CAST( MONTH(@StartDate) AS VARCHAR(10) ) AS INT), 
-			DATENAME( month , DateAdd( month , MONTH(@StartDate) , 0 ) - 1 ), YEAR(@StartDate) )
-			SET @StartDate = DATEADD(MM, 1, @StartDate)
-		END
+	BEGIN
+		INSERT INTO dbo.DimMonth
+		(
+			MonthId, MonthName, Year
+		)
+		VALUES ( CAST ( CAST( YEAR(@StartDate) AS VARCHAR(10) ) + CAST( MONTH(@StartDate) AS VARCHAR(10) ) AS INT), 
+		DATENAME( month , DATEADD( month , MONTH(@StartDate) , 0 ) - 1 ), YEAR(@StartDate) )
+		SET @StartDate = DATEADD(MM, 1, @StartDate)
+	END
 	RETURN 0
 END
