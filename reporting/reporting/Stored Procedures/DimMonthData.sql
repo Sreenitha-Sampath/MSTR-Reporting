344fd0b8dfad214d30f89dc10956600ca8aa939c
Sampath, Sreenitha <sreenitha.sampath07@gmail.com>
2018-02-05 16:56:07 +0530

    20. SSDT - Modified Recently

--- reporting/reporting/Stored Procedures/DimMonthData.sql
+++ reporting/reporting/Stored Procedures/DimMonthData.sql
@@ -2,13 +2,15 @@
 AS
 BEGIN
 	DECLARE @StartDate date= '20100101'
+	DECLARE @monthKey INT
 	WHILE @StartDate <= '20151231'
 	BEGIN
+		SET @monthKey=    [dbo].[MonthKeyGenerator] (@StartDate)
 		INSERT INTO dbo.DimMonth
 		(
 			MonthId, MonthName, Year
 		)
-		VALUES ( CAST ( CAST( YEAR(@StartDate) AS VARCHAR(10) ) + CAST( MONTH(@StartDate) AS VARCHAR(10) ) AS INT), 
+		VALUES ( CAST(@monthKey AS INT), 
 		DATENAME( month , DATEADD( month , MONTH(@StartDate) , 0 ) - 1 ), YEAR(@StartDate) )
 		SET @StartDate = DATEADD(MM, 1, @StartDate)
 	END
